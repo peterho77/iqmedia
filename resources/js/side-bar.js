afa9d8fc6d5b0125d6e7fb1dd5f870bd0795d28d
peterho <hctd123@gmail.com>
2025-07-10 15:55:47 +0700

    sửa thanh sidebar p2

--- resources/js/side-bar.js
+++ resources/js/side-bar.js
@@ -4,14 +4,26 @@ dropdownBtns.forEach((btn) => {
     btn.addEventListener("click", () => {
         btn.nextElementSibling.classList.toggle("show");
         btn.classList.toggle("rotate");
-    })
-})
+    });
+});
 
-// đóng sidebar
+// đóng thanh sidebar
 const sidebar = document.querySelector("#sidebar");
 const toggleSidebar = document.querySelector(".toggle-sidebar-btn");
 
 toggleSidebar.addEventListener("click", () => {
     sidebar.classList.toggle("close");
+    dropdownBtns.forEach((btn) => {
+        btn.nextElementSibling.classList.remove("show");
+    });
     toggleSidebar.classList.toggle("rotate");
-})
+    sidebar.previousElementSibling.classList.toggle("visually-hidden");
+});
+
+// nhấn nút menu mở thanh sidebar
+const mobileToggleNav = document.querySelector(".mobile-toggle-nav");
+
+mobileToggleNav.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+    sidebar.previousElementSibling.classList.toggle("visually-hidden");
+});
