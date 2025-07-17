3f0ec5713be241e283d1979d881e497630df0fc3
peterho <hctd123@gmail.com>
2025-07-17 15:51:24 +0700

    sửa bug FOUC

--- resources/js/side-bar.js
+++ resources/js/side-bar.js
@@ -1,29 +1,21 @@
-const dropdownBtns = document.querySelectorAll(".dropdown-btn");
-
-dropdownBtns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        btn.nextElementSibling.classList.toggle("show");
-        btn.classList.toggle("rotate");
-    });
-});
+// mở submenu trong thanh sidebar 
+window.toggleSubmenu = (btn) => {
+    btn.nextElementSibling.classList.toggle("show");
+    btn.classList.toggle("rotate");
+}
 
 // đóng thanh sidebar
 const sidebar = document.querySelector("#sidebar");
-const toggleSidebar = document.querySelector(".toggle-sidebar-btn");
 
-toggleSidebar.addEventListener("click", () => {
-    sidebar.classList.toggle("close");
-    dropdownBtns.forEach((btn) => {
-        btn.nextElementSibling.classList.remove("show");
-    });
-    toggleSidebar.classList.toggle("rotate");
-    sidebar.previousElementSibling.classList.toggle("visually-hidden");
-});
+window.closeSidebar = (btn) => {
+    sidebar.classList.remove("show");
+    sidebar.previousElementSibling.classList.add("visually-hidden");
+}
 
 // nhấn nút menu mở thanh sidebar
 const mobileToggleNav = document.querySelector(".mobile-toggle-nav");
 
 mobileToggleNav.addEventListener("click", () => {
-    sidebar.classList.toggle("close");
-    sidebar.previousElementSibling.classList.toggle("visually-hidden");
+    sidebar.classList.add("show");
+    sidebar.previousElementSibling.classList.remove("visually-hidden");
 });
